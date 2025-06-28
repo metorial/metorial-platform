bbcbaf4f19341ffdf129e0b3dac43d213f446112
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-06-28 12:49:39 +0200

    Add hub

---
+++ src/mcp-broker/pkg/hub/hub.go
@@ -0,0 +1,58 @@
+package hub
+
+type Hub struct {
+	clients    map[chan any]bool
+	broadcast  chan any
+	register   chan chan any
+	unregister chan chan any
+	bufferSize int
+}
+
+func NewHub(bufferSize int) *Hub {
+	return &Hub{
+		clients:    make(map[chan any]bool),
+		broadcast:  make(chan any, bufferSize),
+		register:   make(chan chan any),
+		unregister: make(chan chan any),
+		bufferSize: bufferSize,
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client)
+			}
+
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client <- message:
+				default:
+					delete(h.clients, client)
+					close(client)
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) Subscribe() chan any {
+	client := make(chan any, h.bufferSize)
+	h.register <- client
+	return client
+}
+
+func (h *Hub) Send(msg any) {
+	h.broadcast <- msg
+}
+
+func (h *Hub) Unsubscribe(client chan any) {
+	h.unregister <- client
+}
