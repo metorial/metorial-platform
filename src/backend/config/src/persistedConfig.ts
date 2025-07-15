e181fdafe94bd55cb07354cab0b2e8245e52e9ef
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-07-15 14:36:42 +0200

    Add persisted configs

---
+++ src/backend/config/src/persistedConfig.ts
@@ -0,0 +1,19 @@
+import { createLocallyCachedFunction } from '@metorial/cache';
+import { db } from '@metorial/db';
+
+export let getDistributedConfig = createLocallyCachedFunction({
+  getHash: (i: void) => 'config',
+  provider: async (i: void) => {
+    let config = await db.metorialConfig.findFirst({
+      where: { id: 'metorial' }
+    });
+    if (config) return config;
+
+    return db.metorialConfig.upsert({
+      where: { id: 'metorial' },
+      update: {},
+      create: {}
+    });
+  },
+  ttlSeconds: 10
+});
