552b7452a90daae2c6b34c2a54a3b9f29d0e1d14
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-07-24 15:11:40 +0200

    Set up service

---
+++ src/services/log/cmd/server/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	sentryUtil "github.com/metorial/metorial/modules/sentry-util"
+	mongoStore "github.com/metorial/metorial/services/usage/internal/mongo-store"
+	"github.com/metorial/metorial/services/usage/internal/repository"
+	"github.com/metorial/metorial/services/usage/internal/service"
+)
+
+func main() {
+	os.Setenv("TZ", "UTC") // Ensure UTC timezone for consistent timestamps
+
+	err := godotenv.Load()
+	if err != nil {
+		// ignore error if .env file is not found
+	}
+
+	sentryUtil.InitSentryIfNeeded()
+	defer sentryUtil.ShutdownSentry()
+
+	rpcAddress := "localhost:5050"
+
+	rpcAddressEnv := os.Getenv("RPC_ADDRESS")
+	if rpcAddressEnv != "" {
+		rpcAddress = rpcAddressEnv
+	}
+
+	mongoURI := mustGetEnv("MONGO_URI")
+	mongoDb := getEnvOrDefault("MONGO_DB", "usage")
+	mongoCollection := getEnvOrDefault("MONGO_COLLECTION", "usage_records")
+
+	store, err := mongoStore.NewMongoStore(context.Background(), mongoURI, mongoDb, mongoCollection)
+	if err != nil {
+		log.Fatalf("Failed to create MongoDB store: %v", err)
+	}
+
+	repo := repository.NewRepository(store)
+
+	service := service.NewService(repo)
+
+	service.Start(rpcAddress)
+
+	// Wait for interrupt signal
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigChan
+
+	log.Println("Received interrupt signal, shutting down...")
+	if err := service.Stop(); err != nil {
+		log.Printf("Error during shutdown: %v", err)
+	}
+
+	if err := store.Close(context.Background()); err != nil {
+		log.Printf("Error during shutdown: %v", err)
+	}
+}
+
+func mustGetEnv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("Environment variable %s is required but not set", key)
+	}
+	return value
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
