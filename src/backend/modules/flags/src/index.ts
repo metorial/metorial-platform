90091b420b1489f689b970b07cb768659057c785
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-08-10 15:16:34 +0200

    Add flags client

--- src/backend/modules/flags/src/index.ts
+++ src/backend/modules/flags/src/index.ts
@@ -1,5 +1,6 @@
 import { combineQueueProcessors } from '@metorial/queue';
 
+export * from './definitions';
 export * from './services';
 
 export let eventQueueProcessor = combineQueueProcessors([]);
