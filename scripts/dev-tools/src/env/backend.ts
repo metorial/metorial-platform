c5440b85df5f14a5a4b37bd83c144b085de57860
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-10-30 08:56:09 -0700

    Set up project for new lambdas

--- scripts/dev-tools/src/env/backend.ts
+++ scripts/dev-tools/src/env/backend.ts
@@ -183,5 +183,11 @@ export let backendEnv: Env = [
   { key: 'DENO_ORGANIZATION_ID', isEnterprise: true },
 
   { key: 'SCM_GITHUB_CLIENT_ID', isEnterprise: false },
-  { key: 'SCM_GITHUB_CLIENT_SECRET', isEnterprise: false }
+  { key: 'SCM_GITHUB_CLIENT_SECRET', isEnterprise: false },
+
+  { key: 'AWS_ACCESS_KEY_ID', isEnterprise: true },
+  { key: 'AWS_SECRET_ACCESS_KEY', isEnterprise: true },
+  { key: 'AWS_REGION', isEnterprise: true },
+  { key: 'AWS_ACCOUNT_ID', isEnterprise: true },
+  { key: 'LAMBDA_DEPLOY_RESOURCE_PREFIX', isEnterprise: true }
 ];
