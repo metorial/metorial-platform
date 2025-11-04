f36253436ec4f1c24dcf525aff1a140e76a1390b
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-11-03 22:23:43 -0800

    Improve api key ux for members

--- src/backend/apis/core/src/middleware/isDashboard.ts
+++ src/backend/apis/core/src/middleware/isDashboard.ts
@@ -1,8 +1,17 @@
 import { ServiceError, unauthorizedError } from '@metorial/error';
 import { apiGroup } from './apiGroup';
+import { organizationGroup } from './organizationGroup';
 
 export let isDashboardGroup = apiGroup.createMiddleware(async ctx => {
   if (ctx.auth.type != 'user' || ctx.auth.machineAccess) {
+    throw new ServiceError(unauthorizedError());
+  }
+
+  return {};
+});
+
+export let isAdminGroup = organizationGroup.createMiddleware(async ctx => {
+  if (ctx.auth.type != 'user' || !ctx.member || ctx.member.role != 'admin') {
     throw new ServiceError(unauthorizedError());
   }
 
