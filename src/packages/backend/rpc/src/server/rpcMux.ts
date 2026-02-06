52543202f4928c301b77c7d3a77034bd932e5768
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-06 14:26:26 +0100

    Update rpcMux.ts

--- src/packages/backend/rpc/src/server/rpcMux.ts
+++ src/packages/backend/rpc/src/server/rpcMux.ts
@@ -104,8 +104,22 @@ export let rpcMux = (
         return new Response(null, { status: 403 });
       }
 
-      let body: any = null;
+      let contentType = req.headers.get('content-type') ?? '';
+      if (
+        !contentType.includes('application/rpc+json') &&
+        !contentType.includes('application/json')
+      ) {
+        return new Response(
+          JSON.stringify(
+            notAcceptableError({
+              message: 'Content-Type must be application/rpc+json'
+            }).toResponse()
+          ),
+          { status: 406, headers: corsHeaders }
+        );
+      }
 
+      let body: any = null;
       try {
         body = serialize.decode(await req.text());
       } catch (e) {
