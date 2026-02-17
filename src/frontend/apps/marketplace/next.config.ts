0a69499aee6a3d750f315ccf9ecab23c96a767e0
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-17 12:23:05 +0100

    cleanup - wip

--- src/frontend/apps/marketplace/next.config.ts
+++ src/frontend/apps/marketplace/next.config.ts
@@ -8,7 +8,6 @@ const nextConfig: NextConfig = {
     PRIVATE_API_URL: process.env.PRIVATE_API_URL ?? process.env.VITE_PRIVATE_API_URL,
     ADMIN_API_URL: process.env.ADMIN_API_URL ?? process.env.VITE_ADMIN_API_URL,
     AUTH_API_URL: process.env.AUTH_API_URL ?? process.env.VITE_AUTH_API_URL,
-    LANDING_API_URL: process.env.LANDING_API_URL ?? process.env.VITE_LANDING_API_URL,
     TEAM_API_URL: process.env.TEAM_API_URL ?? process.env.VITE_TEAM_API_URL,
     USER_API_URL: process.env.USER_API_URL ?? process.env.VITE_USER_API_URL,
     MARKETPLACE_API_URL:
