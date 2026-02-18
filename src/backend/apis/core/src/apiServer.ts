01d1c965bc095241ad3b10cab6dea16c438ae653
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-18 17:42:19 +0100

    cleanup pulsar registration

--- src/backend/apis/core/src/apiServer.ts
+++ src/backend/apis/core/src/apiServer.ts
@@ -1,26 +1,8 @@
-import {
-  fullDashboardController,
-  magnetarController,
-  magnetarDashboardController,
-  pulsarController
-} from './controllers';
+import { fullDashboardController, magnetarController } from './controllers';
 import { restServer } from './rest';
 
 export let apiServer = restServer.launch({
   versions: {
-    mt_2025_01_01_pulsar: {
-      displayVersion: '2025-01-01',
-      apiVersion: 'mt_2025_01_01_pulsar',
-      alternativeIdentifiers: [
-        'pulsar',
-        '2025_01_01',
-        '2025-01-01',
-        'mt_2025_01_01_pulsar',
-        '2025-01-01-pulsar'
-      ],
-      controller: pulsarController
-    },
-
     mt_2025_01_01_dashboard: {
       displayVersion: '2025-01-01-dashboard',
       apiVersion: 'mt_2025_01_01_dashboard',
@@ -39,13 +21,6 @@ export let apiServer = restServer.launch({
         '2026-02-01-magnetar'
       ],
       controller: magnetarController
-    },
-
-    mt_2025_01_01_dashboard: {
-      displayVersion: '2026-02-01-dashboard',
-      apiVersion: 'mt_2025_01_01_dashboard',
-      alternativeIdentifiers: ['mt_2025_01_01_dashboard', '2026-02-01-dashboard'],
-      controller: magnetarDashboardController
     }
   },
   currentVersion: 'mt_2026_01_01_magnetar'
