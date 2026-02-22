5a4c90134f17a3ad10227b604f6711e0614685a4
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-22 16:41:07 +0100

    Fix proxy url

--- src/backend/modules/subspace/src/proxy/index.ts
+++ src/backend/modules/subspace/src/proxy/index.ts
@@ -5,10 +5,9 @@ import { getTenantForSubspace } from '../subspace';
 
 let baseConnectionUrl = env.subspace.SUBSPACE_CONNECTION_URL;
 
-let getSubspaceMcpUrl = async (instance: Instance, sessionId: string, rawUrl: string) => {
+let getSubspaceMcpUrl = async (instance: Instance, sessionId: string, inputUrl: URL) => {
   let { tenant, solution } = await getTenantForSubspace(instance);
 
-  let inputUrl = new URL(rawUrl);
   return `${baseConnectionUrl}/${solution.id}/${tenant.id}/sessions/${sessionId}/mcp${inputUrl.search}`;
 };
 
@@ -17,10 +16,16 @@ export let proxyMcpRequestToSubspace = async (
   instance: Instance,
   sessionId: string
 ): Promise<Response> => {
-  let subspaceUrl = await getSubspaceMcpUrl(instance, sessionId, c.req.url);
+  let inputUrl = new URL(c.req.url);
+  let subspaceUrl = await getSubspaceMcpUrl(instance, sessionId, inputUrl);
 
   let headers = new Headers(c.req.raw.headers);
-  headers.set('Metorial-Proxy-URL', c.req.url);
+  headers.set(
+    'Metorial-Proxy-URL',
+    process.env.NODE_ENV == 'production'
+      ? `https://${inputUrl.hostname}${inputUrl.pathname}${inputUrl.search}`
+      : `http://${inputUrl.host}${inputUrl.pathname}${inputUrl.search}`
+  );
 
   let response = await fetch(subspaceUrl, {
     method: c.req.method,
