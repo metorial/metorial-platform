3e9bb397c1f072f92a647099882252d3c3e76072
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-03-24 14:27:58 +0100

    welp

--- src/systems/signal/clients/typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@lowerdeck/rpc-client';
-import type { SignalClient } from '../../../service/src/controllers';
-
-type ClientOpts = Parameters<typeof createClient>[0];
-
-export let createSignalClient = (o: ClientOpts): SignalClient => createClient<SignalClient>(o);
--- src/systems/subspace/clients/origin/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@lowerdeck/rpc-client';
-import { ClientOpts } from '@lowerdeck/rpc-client/dist/shared/clientBuilder';
-import type { OriginClient } from '../../../apps/service/src/controllers';
-
-export let createOriginClient = (o: ClientOpts) => createClient<OriginClient>(o);
