4bcf2690efc34cb740525d72886c85c50bd2b741
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-03-24 20:38:29 +0100

    Update dev env

--- scripts/dev-tools/scripts/dbInit.sh
+++ scripts/dev-tools/scripts/dbInit.sh
@@ -10,6 +10,13 @@ export MAIN_DATABASE_URL="postgres://postgres:postgres@localhost:35432/$DB_PREFI
 export MAIN_PAYMENT_DATABASE_URL="postgres://postgres:postgres@localhost:35432/$DB_PREFIX-payment"
 export MAIN_FEDERATION_CORE_DATABASE_URL="postgres://postgres:postgres@localhost:35432/$DB_PREFIX-federation"
 
+export ORIGIN_DATABASE_URL="postgres://postgres:postgres@localhost:35432/origin"
+export SIGNAL_DATABASE_URL="postgres://postgres:postgres@localhost:35432/signal"
+export SLATES_HUB_DATABASE_URL="postgres://postgres:postgres@localhost:35432/slates-hub"
+export SUBSPACE_DATABASE_URL="postgres://postgres:postgres@localhost:35432/subspace"
+export SHUTTLE_DATABASE_URL="postgres://postgres:postgres@localhost:35432/shuttle"
+export HORIZON_DATABASE_URL="postgres://postgres:postgres@localhost:35432/horizon"
+
 export MIGRATION_DATABASE_URL="postgres://postgres:postgres@localhost:35432/migrate-$DB_PREFIX"
 export MIGRATION_PAYMENT_DATABASE_URL="postgres://postgres:postgres@localhost:35432/migrate-$DB_PREFIX-payment"
 export MIGRATION_FEDERATION_CORE_DATABASE_URL="postgres://postgres:postgres@localhost:35432/migrate-$DB_PREFIX-federation"
