3d48cc222a239654febd48071bd14a6be6c066a2
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-03-27 09:55:33 +0100

    Index provider capabilities

--- src/systems/subspace/provider-backends/provider-utils/src/providerTypes.ts
+++ src/systems/subspace/provider-backends/provider-utils/src/providerTypes.ts
@@ -9,16 +9,35 @@ export let ensureProviderType = async (
 ) => {
   let identifier = `provider::type::${await Hash.sha256(canonicalize({ attributes }))}`;
 
+  let inner = {
+    identifier,
+    name,
+    attributes,
+
+    supportsConfig: attributes.config.status == 'enabled',
+    supportsAuth: attributes.auth.status == 'enabled',
+    supportsOAuth:
+      attributes.auth.status == 'enabled' && attributes.auth.oauth.status == 'enabled',
+    supportsOAuthAutoRegistration:
+      attributes.auth.status == 'enabled' &&
+      attributes.auth.oauth.status == 'enabled' &&
+      attributes.auth.oauth.oauthAutoRegistration?.status == 'supported',
+    supportsCallbacks: attributes.triggers.status == 'enabled',
+    supportsAuthExport:
+      attributes.auth.status == 'enabled' && attributes.auth.export.status == 'enabled',
+    supportsAuthImport:
+      attributes.auth.status == 'enabled' && attributes.auth.import.status == 'enabled'
+  };
+
   return await db.providerType.upsert({
     where: { identifier },
-    update: { name, attributes },
+    update: inner,
     create: {
       oid: get4ByteIntId(),
       id: await ID.generateId('providerType'),
-      identifier,
-      name,
-      attributes,
-      shortKey: generatePlainId(3).toLowerCase()
+      shortKey: generatePlainId(3).toLowerCase(),
+
+      ...inner
     }
   });
 };
