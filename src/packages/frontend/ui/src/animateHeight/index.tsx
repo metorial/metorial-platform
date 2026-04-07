0af0a0bba0abf7ab723580ba54d2dea9c8976da7
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-04-07 11:28:09 +0200

    Safari sucks so it cannot have animations

--- src/packages/frontend/ui/src/animateHeight/index.tsx
+++ src/packages/frontend/ui/src/animateHeight/index.tsx
@@ -2,8 +2,16 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { useMeasure } from 'react-use';
 
+let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 export let AnimateHeight = (props: { children: React.ReactNode }) => {
   let [ref, { height }] = useMeasure();
+
+  if (isSafari) {
+    // Safari has a weird bug where it doesn't properly animate height when the content changes.
+    // This is a workaround that forces Safari to re-render the component when the content changes.
+    return <div>{props.children}</div>;
+  }
 
   return (
     <motion.div
