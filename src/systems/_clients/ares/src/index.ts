bd5bdaee162012437306024251340f23f5127d65
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-04-14 19:50:00 +0200

    Update index.ts

--- src/systems/_clients/ares/src/index.ts
+++ src/systems/_clients/ares/src/index.ts
@@ -1,13 +1,7 @@
 import { createClient } from '@lowerdeck/rpc-client';
-import type { AuthClient } from '../../../ares/service/src/apis/auth/controllers';
 import type { InternalClient } from '../../../ares/service/src/apis/internal';
 
 type ClientOpts = Parameters<typeof createClient>[0];
 
 export let createAresInternalClient = (o: ClientOpts): InternalClient =>
   createClient<InternalClient>(o);
-
-export let createAresAuthClient = (o: ClientOpts) =>
-  createClient<{
-    oauth: AuthClient['oauth'];
-  }>(o);
