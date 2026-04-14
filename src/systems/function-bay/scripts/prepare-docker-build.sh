96fdf3c9d3d2f5993ad4d02a16de726ebc2fab47
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-04-14 17:01:35 +0200

    More fixes

--- src/systems/function-bay/scripts/prepare-docker-build.sh
+++ src/systems/function-bay/scripts/prepare-docker-build.sh
@@ -9,3 +9,4 @@ bun install --linker=hoisted
 
 bun run --cwd ./src/systems/forge/service db:generate
 bunx turbo run --ui=stream build --filter=@metorial-platform-systems/forge-client
+bunx turbo run --ui=stream build --filter=./src/systems/function-bay/packages/**
