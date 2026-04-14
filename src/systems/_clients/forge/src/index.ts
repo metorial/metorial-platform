a702267e34fcdbb5de1c0a3a5788ba50f99db37a
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-04-14 13:43:21 +0200

    Fixes

--- src/systems/_clients/forge/src/index.ts
+++ src/systems/_clients/forge/src/index.ts
@@ -3,4 +3,4 @@ import type { ForgeClient } from '../../../forge/service/src/controllers';
 
 type ClientOpts = Parameters<typeof createClient>[0];
 
-export let createForgeClient = (o: ClientOpts) => createClient<ForgeClient>(o);
+export let createForgeClient = (o: ClientOpts): ForgeClient => createClient<ForgeClient>(o);
