95b6d1e6497935c9179bb3b25c316acbac73a926
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-04-15 12:47:29 +0200

    Fix build

--- src/systems/shuttle/scripts/prepare-docker-build.sh
+++ src/systems/shuttle/scripts/prepare-docker-build.sh
@@ -12,4 +12,4 @@ bun run --cwd ./src/systems/function-bay/service db:generate
 
 bunx turbo run --ui=stream build --filter=@metorial-platform-systems/forge-client
 bunx turbo run --ui=stream build --filter=@metorial-platform-systems/function-bay-client
-bunx turbo run --ui=stream build --filter=./src/systems/shuttle/sdk
+bunx turbo run --ui=stream build --filter=@metorial/mcp-server
