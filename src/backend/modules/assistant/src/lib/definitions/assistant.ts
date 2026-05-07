b61b7cb4ea2be22bf41e5635ad899292600b2631
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-05-07 18:40:32 +0200

    Add definition management

---
+++ src/backend/modules/assistant/src/lib/definitions/assistant.ts
@@ -0,0 +1,39 @@
+import { AssistantOwner, db, ID } from '@metorial/db';
+import { Implementation } from './implementation';
+
+export let assistant = async (d: {
+  slug: string;
+  name: string;
+  systemIdentifier: string | undefined;
+  implementation: Promise<Implementation>;
+}) => {
+  let implementation = await d.implementation;
+
+  let _persisted = await db.assistant.upsert({
+    where: {
+      slug: d.slug
+    },
+    update: {
+      name: d.name,
+      systemIdentifier: d.systemIdentifier,
+      implementationOid: implementation._persisted.oid
+    },
+    create: {
+      id: await ID.generateId('assistant'),
+      ownerType: AssistantOwner.metorial,
+      implementationOid: implementation._persisted.oid,
+      name: d.name,
+      slug: d.slug,
+      systemIdentifier: d.systemIdentifier
+    },
+    include: {
+      implementation: true
+    }
+  });
+
+  return {
+    _persisted,
+    ...d,
+    implementation
+  };
+};
---
+++ src/backend/modules/assistant/src/lib/definitions/index.ts
@@ -0,0 +1,6 @@
+export * from './assistant';
+export * from './implementation';
+export * from './model';
+export * from './models';
+export * from './provider';
+export * from './sandbox';
