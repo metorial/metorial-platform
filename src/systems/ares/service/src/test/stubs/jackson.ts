86416def648c619dd38c1879fa4eca9c4a700fd3
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-05-22 15:58:07 +0200

    Remove stubs

--- src/systems/ares/service/src/test/stubs/jackson.ts
+++
@@ -1,9 +0,0 @@
-export let jackson = {
-  apiController: {},
-  oauthController: {},
-  redirectUrl: 'http://ares-sso.test/*',
-  defaultRedirectUrl: {
-    saml: 'http://ares-sso.test/sso/jxn/saml/callback',
-    oidc: 'http://ares-sso.test/sso/jxn/oidc/callback'
-  }
-};
