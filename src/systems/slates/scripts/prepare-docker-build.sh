0622e2ace6defe13b8fd3157539f5c2d896c936b
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-05-26 16:49:04 +0200

    Build nebula client

--- src/systems/slates/scripts/prepare-docker-build.sh
+++ src/systems/slates/scripts/prepare-docker-build.sh
@@ -11,9 +11,11 @@ bun run --cwd ./src/systems/forge/service db:generate
 bun run --cwd ./src/systems/function-bay/service db:generate
 bun run --cwd ./src/systems/signal/service db:generate
 bun run --cwd ./src/systems/ares/service db:generate
+bun run --cwd ./src/systems/nebula/service db:generate
 
 bunx turbo run --ui=stream build --filter=@metorial-platform-systems/ares-client
 bunx turbo run --ui=stream build --filter=@metorial-platform-systems/forge-client
 bunx turbo run --ui=stream build --filter=@metorial-platform-systems/function-bay-client
 bunx turbo run --ui=stream build --filter=@metorial-platform-systems/signal-client
+bunx turbo run --ui=stream build --filter=@metorial-platform-systems/nebula-client
 # bunx turbo run --ui=stream build --filter=@slates/proto
