ff58e76f16b218e36ef7a5abe6d33debfaa4d7ce
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-05-30 10:22:00 +0200

    Patch backends for Instance configs

---
+++ src/systems/subspace/provider-backends/provider-shuttle/src/impl/enclaveInstanceConfiguration.ts
@@ -0,0 +1,43 @@
+import {
+  IProviderEnclaveInstanceConfiguration,
+  type ProviderEnclaveInstanceConfigurationSyncParam
+} from '@metorial-subspace/provider-utils';
+import { db, getId } from '@metorial-subspace/db';
+import { getTenantForShuttle, shuttle } from '../client';
+
+export class ProviderEnclaveInstanceConfiguration extends IProviderEnclaveInstanceConfiguration {
+  override async syncEnclaveInstanceConfiguration(
+    data: ProviderEnclaveInstanceConfigurationSyncParam
+  ): Promise<void> {
+    let providerDeployment = await db.providerDeployment.findUniqueOrThrow({
+      where: { oid: data.providerDeployment.oid },
+      include: { serverInstanceConfiguration: true }
+    });
+    let tenant = await getTenantForShuttle(data.tenant);
+
+    let configuration = await shuttle.serverInstanceConfiguration.upsert({
+      tenantId: tenant.id,
+      serverInstanceConfigurationId: providerDeployment.serverInstanceConfiguration?.id,
+      enclaveId: data.enclaveId,
+      egressPolicy: data.egressPolicy
+    });
+
+    let link =
+      providerDeployment.serverInstanceConfiguration ??
+      (await db.serverInstanceConfiguration.create({
+        data: {
+          ...getId('serverInstanceConfiguration'),
+          id: configuration.id,
+          tenantOid: data.providerDeployment.tenantOid,
+          environmentOid: data.providerDeployment.environmentOid
+        }
+      }));
+
+    if (!providerDeployment.serverInstanceConfigurationOid) {
+      await db.providerDeployment.updateMany({
+        where: { oid: providerDeployment.oid },
+        data: { serverInstanceConfigurationOid: link.oid }
+      });
+    }
+  }
+}
