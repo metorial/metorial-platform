a1817c63cd746457e300bf773933287bb46b2473
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-06-02 20:00:01 +0200

    Fix typo

--- src/backend/modules/organization/src/email/invite.tsx
+++ src/backend/modules/organization/src/email/invite.tsx
@@ -19,11 +19,11 @@ export let sendOrgInviteEmail = notificationClient.createTemplate(
 
       if (invite.action === 'onboarding') {
         return createEmail({
-          subject: `Welcome to Metorial ${organization.name}!`,
+          subject: `Welcome to Metorial, ${organization.name}!`,
           preview: `We have set up your organization, ${organization.name}, on Metorial. Click the button below to get started and complete the onboarding process.`,
           content: (
             <Layout
-              title={`Welcome to Metorial ${organization.name}!`}
+              title={`Welcome to Metorial, ${organization.name}!`}
               description={`We have set up your organization, ${organization.name}, on Metorial. Click the button below to get started and complete the onboarding process.`}
             >
               <Button href={url.toString()}>Get Started</Button>
