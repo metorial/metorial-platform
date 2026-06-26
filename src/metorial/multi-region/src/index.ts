f08cdbde2c67518d0b0d26e976d7c605bc9ce8b2
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-06-26 09:36:05 +0200

    Add global profiles

--- src/metorial/multi-region/src/index.ts
+++ src/metorial/multi-region/src/index.ts
@@ -2,12 +2,12 @@ import { combineQueueProcessors } from '@metorial/queue';
 import { fromDeploymentSyncProcessors } from './sync/fromDeployment';
 import { toDeploymentSyncProcessors } from './sync/toDeployment';
 
-export * from './repositories';
 export * from './cell';
+export * from './repositories';
 
 export let multiRegionQueueProcessor = combineQueueProcessors([
   toDeploymentSyncProcessors,
   fromDeploymentSyncProcessors
 ]);
 
-export type { Cell, CellToken } from './db';
+export * from './db';
