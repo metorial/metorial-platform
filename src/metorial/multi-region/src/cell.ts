2a37c6edf4d3cb00d02afee34a1ac60c35972023
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-08-06 20:49:57 +0200

    Harden cell reclaim

--- src/metorial/multi-region/src/cell.ts
+++ src/metorial/multi-region/src/cell.ts
@@ -9,15 +9,26 @@ let getSecureRandomInt = () => {
   return array[0] & 0x7fffffff;
 };
 
-export let cell = ensureGlobalDatabaseReady().then(
-  async () =>
-    await globalDB.cell.upsert({
-      where: { identifier: deploymentIdentifier },
-      create: {
-        identifier: deploymentIdentifier,
-        oid: getSecureRandomInt(),
-        endpointUrl: env.service.EXTERNAL_MULTI_REGION_ENDPOINT
+export let cell = ensureGlobalDatabaseReady().then(async () => {
+  let cell = await globalDB.cell.upsert({
+    where: { identifier: deploymentIdentifier },
+    create: {
+      identifier: deploymentIdentifier,
+      oid: getSecureRandomInt(),
+      endpointUrl: env.service.EXTERNAL_MULTI_REGION_ENDPOINT
+    },
+    update: { endpointUrl: env.service.EXTERNAL_MULTI_REGION_ENDPOINT }
+  });
+
+  if (env.service.EXTERNAL_MULTI_REGION_ENDPOINT) {
+    await globalDB.cell.updateMany({
+      where: {
+        endpointUrl: env.service.EXTERNAL_MULTI_REGION_ENDPOINT,
+        identifier: { not: deploymentIdentifier }
       },
-      update: { endpointUrl: env.service.EXTERNAL_MULTI_REGION_ENDPOINT }
-    })
-);
+      data: { endpointUrl: null }
+    });
+  }
+
+  return cell;
+});
