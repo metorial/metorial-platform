8b66d05448b793b4bb879b568b321b47ad001736
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-08-06 14:08:06 +0200

    Fixes

--- src/metorial/modules/access/src/env.ts
+++ src/metorial/modules/access/src/env.ts
@@ -3,6 +3,6 @@ import { v } from '@lowerdeck/validation';
 
 export let env = createValidatedEnv({
   service: {
-    METORIAL_REGION: v.optional(v.enumOf(['us1', 'eu1']))
+    METORIAL_REGION: v.optional(v.enumOf(['us1', 'eu1', 'dev']))
   }
 });
--- src/metorial/multi-region/src/env.ts
+++ src/metorial/multi-region/src/env.ts
@@ -3,7 +3,7 @@ import { v } from '@lowerdeck/validation';
 
 export let env = createValidatedEnv({
   service: {
-    METORIAL_REGION: v.optional(v.enumOf(['us1', 'eu1'])),
+    METORIAL_REGION: v.optional(v.enumOf(['us1', 'eu1', 'dev'])),
     GLOBAL_DB_TRANSACTION_MAX_WAIT_MS: v.optional(v.number()),
     GLOBAL_DB_TRANSACTION_TIMEOUT_MS: v.optional(v.number()),
     GLOBAL_DB_KEEPALIVE_INTERVAL_MS: v.optional(v.number()),
