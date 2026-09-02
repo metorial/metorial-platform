f1a18b919e92252eb45342d29850d314ed95e35a
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-09-02 16:32:43 +0200

    Add outpost models

--- src/metorial/db/src/id.ts
+++ src/metorial/db/src/id.ts
@@ -24,6 +24,15 @@ export let ID = createIdGenerator({
   organizationLayout: idType.sorted('oly'),
   account: idType.sorted('acc'),
 
+  outpost: idType.sorted('otp_'),
+  outpostCredential: idType.sorted('otc_'),
+  outpostAccess: idType.sorted('ota_'),
+  outpostInstance: idType.sorted('otn_'),
+  outpostInstanceService: idType.sorted('otsv_'),
+  outpostInstanceKeyRotation: idType.sorted('otr_'),
+  outpostInstanceEvent: idType.sorted('ote_'),
+  outpostTokenKeyPair: idType.sorted('otkp_'),
+
   project: idType.sorted('prj'),
   projectBrand: idType.sorted('pbr'),
   projectBrandUpdate: idType.sorted('pbu'),
