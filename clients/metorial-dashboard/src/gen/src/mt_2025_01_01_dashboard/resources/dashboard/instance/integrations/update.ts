3649b0c3bac5e3aeab18b60cb755e0d075299dc6
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-09-07 16:04:09 +0200

    Generate clients

--- clients/metorial-dashboard/src/gen/src/mt_2025_01_01_dashboard/resources/dashboard/instance/integrations/update.ts
+++ clients/metorial-dashboard/src/gen/src/mt_2025_01_01_dashboard/resources/dashboard/instance/integrations/update.ts
@@ -56,8 +56,42 @@ export type DashboardInstanceIntegrationsUpdateOutput = {
       createdAt: Date;
       updatedAt: Date;
     } | null;
-    callbacksEnabled: boolean;
-    callbackId: string | null;
+    callbacks: {
+      object: 'integration.provider.callbacks';
+      status: 'enabled' | 'disabled';
+      callbackId: string | null;
+      callback: {
+        object: 'callback';
+        id: string;
+        status: 'active' | 'archived' | 'deleted';
+        name: string;
+        description: string | null;
+        metadata: Record<string, any> | null;
+        integrationId: string;
+        integrationProviderId: string;
+        provider: {
+          object: 'provider#preview';
+          id: string;
+          name: string;
+          description: string | null;
+          slug: string;
+          createdAt: Date;
+          updatedAt: Date;
+        };
+        createdAt: Date;
+        updatedAt: Date;
+        sync: {
+          object: 'callback.sync';
+          status: 'pending' | 'synced' | 'failed';
+          error: {
+            object: 'callback.sync.error';
+            code: string;
+            message: string | null;
+          } | null;
+          syncedAt: Date | null;
+        };
+      } | null;
+    };
     createdAt: Date;
     updatedAt: Date;
     archivedAt: Date | null;
@@ -117,37 +151,6 @@ export type DashboardInstanceIntegrationsUpdateOutput = {
       metadata: Record<string, any> | null;
       scopes: string[] | null;
       providerId: string;
-      createdAt: Date;
-      updatedAt: Date;
-    } | null;
-    callback: {
-      object: 'callback';
-      id: string;
-      status: 'active' | 'archived' | 'deleted';
-      sync: {
-        object: 'callback.sync';
-        status: 'pending' | 'synced' | 'failed';
-        error: {
-          object: 'callback.sync.error';
-          code: string;
-          message: string | null;
-        } | null;
-        syncedAt: Date | null;
-      };
-      name: string;
-      description: string | null;
-      metadata: Record<string, any> | null;
-      integrationId: string;
-      integrationProviderId: string;
-      provider: {
-        object: 'provider#preview';
-        id: string;
-        name: string;
-        description: string | null;
-        slug: string;
-        createdAt: Date;
-        updatedAt: Date;
-      };
       createdAt: Date;
       updatedAt: Date;
     } | null;
@@ -290,11 +293,75 @@ export let mapDashboardInstanceIntegrationsUpdateOutput =
               updatedAt: mtMap.objectField('updated_at', mtMap.date())
             })
           ),
-          callbacksEnabled: mtMap.objectField(
-            'callbacks_enabled',
-            mtMap.passthrough()
+          callbacks: mtMap.objectField(
+            'callbacks',
+            mtMap.object({
+              object: mtMap.objectField('object', mtMap.passthrough()),
+              status: mtMap.objectField('status', mtMap.passthrough()),
+              callbackId: mtMap.objectField('callback_id', mtMap.passthrough()),
+              callback: mtMap.objectField(
+                'callback',
+                mtMap.object({
+                  object: mtMap.objectField('object', mtMap.passthrough()),
+                  id: mtMap.objectField('id', mtMap.passthrough()),
+                  status: mtMap.objectField('status', mtMap.passthrough()),
+                  name: mtMap.objectField('name', mtMap.passthrough()),
+                  description: mtMap.objectField(
+                    'description',
+                    mtMap.passthrough()
+                  ),
+                  metadata: mtMap.objectField('metadata', mtMap.passthrough()),
+                  integrationId: mtMap.objectField(
+                    'integration_id',
+                    mtMap.passthrough()
+                  ),
+                  integrationProviderId: mtMap.objectField(
+                    'integration_provider_id',
+                    mtMap.passthrough()
+                  ),
+                  provider: mtMap.objectField(
+                    'provider',
+                    mtMap.object({
+                      object: mtMap.objectField('object', mtMap.passthrough()),
+                      id: mtMap.objectField('id', mtMap.passthrough()),
+                      name: mtMap.objectField('name', mtMap.passthrough()),
+                      description: mtMap.objectField(
+                        'description',
+                        mtMap.passthrough()
+                      ),
+                      slug: mtMap.objectField('slug', mtMap.passthrough()),
+                      createdAt: mtMap.objectField('created_at', mtMap.date()),
+                      updatedAt: mtMap.objectField('updated_at', mtMap.date())
+                    })
+                  ),
+                  createdAt: mtMap.objectField('created_at', mtMap.date()),
+                  updatedAt: mtMap.objectField('updated_at', mtMap.date()),
+                  sync: mtMap.objectField(
+                    'sync',
+                    mtMap.object({
+                      object: mtMap.objectField('object', mtMap.passthrough()),
+                      status: mtMap.objectField('status', mtMap.passthrough()),
+                      error: mtMap.objectField(
+                        'error',
+                        mtMap.object({
+                          object: mtMap.objectField(
+                            'object',
+                            mtMap.passthrough()
+                          ),
+                          code: mtMap.objectField('code', mtMap.passthrough()),
+                          message: mtMap.objectField(
+                            'message',
+                            mtMap.passthrough()
+                          )
+                        })
+                      ),
+                      syncedAt: mtMap.objectField('synced_at', mtMap.date())
+                    })
+                  )
+                })
+              )
+            })
           ),
-          callbackId: mtMap.objectField('callback_id', mtMap.passthrough()),
           createdAt: mtMap.objectField('created_at', mtMap.date()),
           updatedAt: mtMap.objectField('updated_at', mtMap.date()),
           archivedAt: mtMap.objectField('archived_at', mtMap.date()),
@@ -404,61 +471,6 @@ export let mapDashboardInstanceIntegrationsUpdateOutput =
                 mtMap.array(mtMap.passthrough())
               ),
               providerId: mtMap.objectField('provider_id', mtMap.passthrough()),
-              createdAt: mtMap.objectField('created_at', mtMap.date()),
-              updatedAt: mtMap.objectField('updated_at', mtMap.date())
-            })
-          ),
-          callback: mtMap.objectField(
-            'callback',
-            mtMap.object({
-              object: mtMap.objectField('object', mtMap.passthrough()),
-              id: mtMap.objectField('id', mtMap.passthrough()),
-              status: mtMap.objectField('status', mtMap.passthrough()),
-              sync: mtMap.objectField(
-                'sync',
-                mtMap.object({
-                  object: mtMap.objectField('object', mtMap.passthrough()),
-                  status: mtMap.objectField('status', mtMap.passthrough()),
-                  error: mtMap.objectField(
-                    'error',
-                    mtMap.object({
-                      object: mtMap.objectField('object', mtMap.passthrough()),
-                      code: mtMap.objectField('code', mtMap.passthrough()),
-                      message: mtMap.objectField('message', mtMap.passthrough())
-                    })
-                  ),
-                  syncedAt: mtMap.objectField('synced_at', mtMap.date())
-                })
-              ),
-              name: mtMap.objectField('name', mtMap.passthrough()),
-              description: mtMap.objectField(
-                'description',
-                mtMap.passthrough()
-              ),
-              metadata: mtMap.objectField('metadata', mtMap.passthrough()),
-              integrationId: mtMap.objectField(
-                'integration_id',
-                mtMap.passthrough()
-              ),
-              integrationProviderId: mtMap.objectField(
-                'integration_provider_id',
-                mtMap.passthrough()
-              ),
-              provider: mtMap.objectField(
-                'provider',
-                mtMap.object({
-                  object: mtMap.objectField('object', mtMap.passthrough()),
-                  id: mtMap.objectField('id', mtMap.passthrough()),
-                  name: mtMap.objectField('name', mtMap.passthrough()),
-                  description: mtMap.objectField(
-                    'description',
-                    mtMap.passthrough()
-                  ),
-                  slug: mtMap.objectField('slug', mtMap.passthrough()),
-                  createdAt: mtMap.objectField('created_at', mtMap.date()),
-                  updatedAt: mtMap.objectField('updated_at', mtMap.date())
-                })
-              ),
               createdAt: mtMap.objectField('created_at', mtMap.date()),
               updatedAt: mtMap.objectField('updated_at', mtMap.date())
             })
