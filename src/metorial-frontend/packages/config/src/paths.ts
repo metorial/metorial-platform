29972e811f05285d88dc820bd69c63e3a8302946
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-09-10 12:35:14 +0200

    Add documents slice

--- src/metorial-frontend/packages/config/src/paths.ts
+++ src/metorial-frontend/packages/config/src/paths.ts
@@ -1021,6 +1021,19 @@ let SupportPaths = Object.assign(
   }
 );
 
+let DocumentsPaths = Object.assign(
+  (...subPages: SubPages) => getNexusUrl('documents', () => joinPaths(...subPages)),
+  {
+    organization: (
+      organizationId: string | null | undefined,
+      ...subPages: SubPages
+    ) => {
+      if (!organizationId) return '#';
+      return DocumentsPaths(organizationId, 'documents', ...subPages);
+    }
+  }
+);
+
 let EnterprisePaths = Object.assign(
   (accountId: string | null | undefined, ...subPages: SubPages) =>
     getNexusUrl('enterprise', () => {
@@ -1885,6 +1898,7 @@ export let Paths = {
 
   instance: InstancePaths,
   account: AccountPaths,
+  documents: DocumentsPaths,
   support: SupportPaths,
   enterprise: EnterprisePaths,
   organization: OrganizationPaths,
