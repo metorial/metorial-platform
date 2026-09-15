3592e1216df25e1583afc923caaf963e10fdbfca
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-09-15 09:35:59 +0200

    Add chat state

---
+++ src/metorial-frontend/packages/state/src/chat/index.ts
@@ -0,0 +1,5 @@
+export * from './loaders/chatConnections';
+export * from './loaders/chatEvents';
+export * from './loaders/chatInstances';
+export * from './loaders/chatWorkspaces';
+export * from './loaders/chats';
