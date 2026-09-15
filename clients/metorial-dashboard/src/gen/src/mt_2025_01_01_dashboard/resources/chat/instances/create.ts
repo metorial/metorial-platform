1b700f5f11eab02c522ed98f8607748af74084a6
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-09-15 10:49:51 +0200

    Generate client

--- clients/metorial-dashboard/src/gen/src/mt_2025_01_01_dashboard/resources/chat/instances/create.ts
+++ clients/metorial-dashboard/src/gen/src/mt_2025_01_01_dashboard/resources/chat/instances/create.ts
@@ -8,6 +8,15 @@ export type ChatInstancesCreateOutput = {
   name: string;
   description: string | null;
   metadata: Record<string, any>;
+  identity: {
+    id: string;
+    userId: string;
+    name: string;
+    username: string;
+    providerType: string;
+    email: string | null;
+    imageUrl: string | null;
+  } | null;
   createdAt: Date;
   updatedAt: Date;
 };
@@ -24,6 +33,18 @@ export let mapChatInstancesCreateOutput =
     name: mtMap.objectField('name', mtMap.passthrough()),
     description: mtMap.objectField('description', mtMap.passthrough()),
     metadata: mtMap.objectField('metadata', mtMap.passthrough()),
+    identity: mtMap.objectField(
+      'identity',
+      mtMap.object({
+        id: mtMap.objectField('id', mtMap.passthrough()),
+        userId: mtMap.objectField('user_id', mtMap.passthrough()),
+        name: mtMap.objectField('name', mtMap.passthrough()),
+        username: mtMap.objectField('username', mtMap.passthrough()),
+        providerType: mtMap.objectField('provider_type', mtMap.passthrough()),
+        email: mtMap.objectField('email', mtMap.passthrough()),
+        imageUrl: mtMap.objectField('image_url', mtMap.passthrough())
+      })
+    ),
     createdAt: mtMap.objectField('created_at', mtMap.date()),
     updatedAt: mtMap.objectField('updated_at', mtMap.date())
   });
--- clients/metorial-dashboard/src/gen/src/mt_2025_01_01_dashboard/resources/management/instance/chat/instances/list.ts
+++ clients/metorial-dashboard/src/gen/src/mt_2025_01_01_dashboard/resources/management/instance/chat/instances/list.ts
@@ -9,6 +9,15 @@ export type ManagementInstanceChatInstancesListOutput = {
     name: string;
     description: string | null;
     metadata: Record<string, any>;
+    identity: {
+      id: string;
+      userId: string;
+      name: string;
+      username: string;
+      providerType: string;
+      email: string | null;
+      imageUrl: string | null;
+    } | null;
     createdAt: Date;
     updatedAt: Date;
   }[];
@@ -31,6 +40,21 @@ export let mapManagementInstanceChatInstancesListOutput =
           name: mtMap.objectField('name', mtMap.passthrough()),
           description: mtMap.objectField('description', mtMap.passthrough()),
           metadata: mtMap.objectField('metadata', mtMap.passthrough()),
+          identity: mtMap.objectField(
+            'identity',
+            mtMap.object({
+              id: mtMap.objectField('id', mtMap.passthrough()),
+              userId: mtMap.objectField('user_id', mtMap.passthrough()),
+              name: mtMap.objectField('name', mtMap.passthrough()),
+              username: mtMap.objectField('username', mtMap.passthrough()),
+              providerType: mtMap.objectField(
+                'provider_type',
+                mtMap.passthrough()
+              ),
+              email: mtMap.objectField('email', mtMap.passthrough()),
+              imageUrl: mtMap.objectField('image_url', mtMap.passthrough())
+            })
+          ),
           createdAt: mtMap.objectField('created_at', mtMap.date()),
           updatedAt: mtMap.objectField('updated_at', mtMap.date())
         })
